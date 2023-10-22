4ed7e1e091484644ea2dd39790665660a831b0a6
JC-78 <43752338+JC-78@users.noreply.github.com>
2023-10-22 14:49:01 -0400

    f

--- tmp/train3d.py
+++ tmp/train3d.py
@@ -11,33 +11,45 @@ data=[]
 labels=[]
 train_val_df = pd.read_csv("data/labels/train_val.csv")
 
+length_frequency = {}
+
 for index,row in train_val_df.iterrows():
     filename=row[0]
     label=row[1]
     pkl=os.path.join('data/cnn3d/',filename+".pkl")
     with open(pkl,'rb') as file:
         pkl_data=pickle.load(file)
-    print(f"Filename: {filename}, Shape: {pkl_data[1].flatten().shape}")
+    # print(f"Filename: {filename}, Shape: {pkl_data[1].flatten().shape}")
     item=pkl_data[1].flatten()
     # print("item shape",item.shape)
     # reshaped_item=np.resize(item,2048)
+    reshaped_item=np.resize(item,6144)
     # print("reshaped item shape",reshaped_item.shape)
-    # data.append(reshaped_item)
+    data.append(reshaped_item)
     #version 2
-    target_length = 2048
+    # target_length = 2048
     
-    if len(item) < target_length:
-        # Pad shorter arrays with zeros
-        padded_item = np.pad(item, (0, target_length - len(item)))
-        data.append(padded_item)
-    elif len(item) > target_length:
-        # Truncate longer arrays
-        truncated_item = item[:target_length]
-        data.append(truncated_item)
-    else:
-        # Keep arrays of the target length
-        data.append(item)
+    # if len(item) < target_length:
+    #     # Pad shorter arrays with zeros
+    #     padded_item = np.pad(item, (0, target_length - len(item)))
+    #     data.append(padded_item)
+    # elif len(item) > target_length:
+    #     # Truncate longer arrays
+    #     truncated_item = item[:target_length]
+    #     data.append(truncated_item)
+    # else:
+    #     # Keep arrays of the target length
+        # data.append(item)
     labels.append(label)
+    length = len(item)
+    if length in length_frequency:
+        length_frequency[length] += 1
+    else:
+        length_frequency[length] = 1
+
+most_common_length = max(length_frequency, key=length_frequency.get)
+print(f"The most common length is {most_common_length} with a frequency of {length_frequency[most_common_length]}")
+
 print("Shape of data:", np.array(data).shape)
 print("Shape of labels:", np.array(labels).shape)
 print("dataset built")
@@ -53,21 +65,21 @@ for index,row in test_df.iterrows():
     with open(pkl,'rb') as file:
         pkl_data=pickle.load(file)
     item=pkl_data[1].flatten()
-    # reshaped_item=np.resize(item,2048)
-    # test_data.append(reshaped_item)
-    target_length = 2048
+    reshaped_item=np.resize(item,2048)
+    test_data.append(reshaped_item)
+    # target_length = 2048
     
-    if len(item) < target_length:
-        # Pad shorter arrays with zeros
-        padded_item = np.pad(item, (0, target_length - len(item)))
-        test_data.append(padded_item)
-    elif len(item) > target_length:
-        # Truncate longer arrays
-        truncated_item = item[:target_length]
-        test_data.append(truncated_item)
-    else:
-        # Keep arrays of the target length
-        test_data.append(item)
+    # if len(item) < target_length:
+    #     # Pad shorter arrays with zeros
+    #     padded_item = np.pad(item, (0, target_length - len(item)))
+    #     test_data.append(padded_item)
+    # elif len(item) > target_length:
+    #     # Truncate longer arrays
+    #     truncated_item = item[:target_length]
+    #     test_data.append(truncated_item)
+    # else:
+    #     # Keep arrays of the target length
+    #     test_data.append(item)
 print("Shape of test data:", np.array(test_data).shape)
 print("predicting")
 pred = rf_classifier.predict(test_data)
