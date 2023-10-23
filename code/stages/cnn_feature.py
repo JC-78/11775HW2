4d88c6374738112f5c9c88c0afa632cba10dda1d
JC-78 <43752338+JC-78@users.noreply.github.com>
2023-10-22 20:41:02 -0400

    f

--- code/stages/cnn_feature.py
+++ code/stages/cnn_feature.py
@@ -19,8 +19,8 @@ class CNNFeature(Stage):
     # def allocate_resource(self, resources, *, model_name='resnet152',
     #                       weight_name='ResNet152_Weights',
     #                       node_name='avgpool', replica_per_gpu=1):
-    def allocate_resource(self, resources, *, model_name='convnetx',
-                          weight_name='ConvNetX_Weights',
+    def allocate_resource(self, resources, *, model_name='ConvNeXt_Large',
+                          weight_name='ConvNeXt_Large_Weights',
                           node_name='avgpool', replica_per_gpu=1):
         self.model_name = model_name
         self.weight_name = weight_name
