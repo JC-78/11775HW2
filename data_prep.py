81ee41b3cd11d7c34f5913337f8ceedbc84ca8d2
JC-78 <43752338+JC-78@users.noreply.github.com>
2023-11-02 12:57:40 -0400

    f

--- data_prep.py
+++ data_prep.py
@@ -4,22 +4,22 @@ import os
 import numpy as np
 
 
-csv="Mzg4MTU4MTQ1OTQ4MzcwOTcxMA==.csv"
-pkl="LTQyMjkzODk1NDU3MjkzOTUwMTA=.pkl"
-print("csv id extraction ",csv[:-3])
-with open(pkl,'rb') as file:
-    pkl_data=pickle.load(file)
-csv_data = np.genfromtxt(csv, delimiter=',').reshape(-1, 1) 
-print(csv_data.shape)
-array1_np = np.array(pkl_data[0])
-array2_np = np.array(pkl_data[1])
-print("array1 (pkl_data[0]) np shape is ",array1_np.shape)
-print("array2 (pkl_data[1]) np shape is",array2_np.shape)
-print("printing content")
-meow = array2_np.reshape(array2_np.shape[0]*array2_np.shape[2]*array2_np.shape[3],1)
-print( "meow.shape",meow.shape)
-combined_data = np.concatenate((csv_data, meow), axis=0)
-print(combined_data.shape)
+# csv="Mzg4MTU4MTQ1OTQ4MzcwOTcxMA==.csv"
+# pkl="LTQyMjkzODk1NDU3MjkzOTUwMTA=.pkl"
+# print("csv id extraction ",csv[:-3])
+# with open(pkl,'rb') as file:
+#     pkl_data=pickle.load(file)
+# csv_data = np.genfromtxt(csv, delimiter=',').reshape(-1, 1) 
+# print(csv_data.shape)
+# array1_np = np.array(pkl_data[0])
+# array2_np = np.array(pkl_data[1])
+# print("array1 (pkl_data[0]) np shape is ",array1_np.shape)
+# print("array2 (pkl_data[1]) np shape is",array2_np.shape)
+# print("printing content")
+# meow = array2_np.reshape(array2_np.shape[0]*array2_np.shape[2]*array2_np.shape[3],1)
+# print( "meow.shape",meow.shape)
+# combined_data = np.concatenate((csv_data, meow), axis=0)
+# print(combined_data.shape)
 
 
 
@@ -31,7 +31,7 @@ for index,row in train_val_df.iterrows():
     filename=row[0]
     label=row[1]
     pkl=os.path.join('11775-HW2/data/cnn3d/',filename+".pkl")
-    csv=os.path.join('11775-HW1/data/snf/',filename+".csv")
+    csv=os.path.join('11775-HW1/snf/',filename+".csv")
     with open(pkl,'rb') as file:
         pkl_data=pickle.load(file)
     csv_data = np.genfromtxt(csv, delimiter=',').reshape(-1, 1) 
@@ -53,7 +53,7 @@ test_data=[]
 test_df=pd.read_csv("data/labels/test_for_students.csv",header=0)
 for index,row in test_df.iterrows():
     filename=row[0]
-    csv=os.path.join('11775-HW1/data/snf/',filename+".csv")
+    csv=os.path.join('11775-HW1/snf/',filename+".csv")
     pkl=os.path.join('data/cnn3d/',filename+".pkl")
     with open(pkl,'rb') as file:
         pkl_data=pickle.load(file)
